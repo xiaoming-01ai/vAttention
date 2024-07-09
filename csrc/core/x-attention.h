c4cb5489f2b123892e8a926f52c0b270800d0677
xiaoming-01ai <xiaoming@01.ai>
2024-07-09 01:51:24 +0000

    init for xAttention

---
+++ csrc/core/x-attention.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+#include <memory>
+#include <cstdint>
+#include <cassert>
+#include <limits>
+#include <string.h>
+#include <iostream>
+
+#define XATTN_NAMESPACE_BEGIN namespace xAttention { 
+#define XATTN_NAMESPACE_END } 
+#define XATTN_NAMESPACE_USE using namespace xAttention;
